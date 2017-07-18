a4b418c6f4cf3aed482d02a7a69dfe4e44e99dd4
fadhilapis <fadhilapis@gmail.com>
2017-07-18 12:35:06 +0800

    add username in user model

--- app/User.php
+++ app/User.php
@@ -17,7 +17,9 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'email', 'password',
+        'email',
+        'password',
+        'username'
     ];
 
     /**
