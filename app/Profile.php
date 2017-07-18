90095e3bac08e5923a5a894e5520429d24524cef
fadhilapis <fadhilapis@gmail.com>
2017-07-18 15:37:02 +0800

    return image url automatically in model

--- app/Profile.php
+++ app/Profile.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\File;
 
 class Profile extends Model
 {
@@ -20,5 +21,19 @@ class Profile extends Model
         'phone_number',
         'profile_picture'
     ];
+
+    public function getProfilePictureAttribute()
+    {
+        $url = url("/photos/default_avatar.png");
+
+        if(!empty($this->profile_picture)) {
+            // If the current profile picture image exist in folder
+            if(File::exists(storage_path('photos/profile_pictures/'.$this->profile_picture))) {
+                $url = url("/photos/".$this->profile_picture);
+            }
+        }
+
+        return $url;
+    }
 
 }
